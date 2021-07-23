543f63a710357ee4ff13893a1ff57a36f38355a9
John Kelly <john@onecleverdude.com>
2021-07-23 14:15:38 -0500

    Showing a quick demo

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # starshiptrainer
 
 A python training project based on an old school basic game.
+
+This will be to help some co-workers get started in understanding some basic coding flows. 
---
+++ starshipcontrol.py
@@ -0,0 +1,20 @@
+def showShip():
+    print("                   ___")
+    print("      ___....-----'---'-----....___")
+    print(" =========================================")
+    print("       ___'---..._______...---'___  ")
+    print("      (___)      _|_|_|_      (___)  ")
+    print("        \\____.-'_.---._'-.____//  ")
+    print("          cccc'.__'---'__.'cccc ")
+    print("                  ccccc")
+
+def main():
+    print("Welcome to the bridge.")
+    showShip()
+    print("\n")
+    print("Right now I can't do much of anything, can I?")
+
+if __name__ == "__main__":
+    main()
+
+
