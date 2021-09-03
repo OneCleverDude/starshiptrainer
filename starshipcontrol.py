b0d0d1b1ed03532a6da34bd252da732c96c228b0
John Kelly <jkelly@incomm.com>
2021-09-03 13:05:22 -0500

    Updating for a quick object thought

--- starshipcontrol.py
+++ starshipcontrol.py
@@ -1,30 +1,46 @@
 import string
 
-def showShip(displayName):
+class Starship:
+    def __init__(self, shipname):
+        self.shipname = shipname
+        self.shields = "down"
+        self.fighters = "docked"
+
+    def renameShip(self, shipname):
+        self.shipname = shipname
+        print ("Ship has been renamed to " + self.shipname)
     
-    # This block has a lot of the code we've looked at.  Try to 
-    # see if you can understand how and why it makes the choices it does.
-    # 
-    # Why pick 20 to subtract from?  Then why 21 later?
-    # Are those the correct numbers?
-    # Why pick the integer division // instead of just a / ?
-    print("                   ___")
-    print("      ___....-----'---'-----....___")
-    if (len(displayName) > 0):
-        shipLine = " "
-        for x in range(20-(len(displayName)//2)):
-            shipLine = shipLine + "="
-        shipLine = shipLine + displayName
-        for x in range(21-(len(displayName)//2)):
-            shipLine = shipLine + "="
-        print(shipLine)
-    else:
-        print(" =========================================")
-    print("       ___'---..._______...---'___  ")
-    print("      (___)      _|_|_|_      (___)  ")
-    print("        \\____.-'_.---._'-.____//  ")
-    print("          cccc'.__'---'__.'cccc ")
-    print("                  ccccc")
+    def launchFighters(self):
+        if self.fighters == "docked":
+            print("Launching fighters!")
+            self.fighters = "launched"
+        else:
+            print("No fighters are docked Commander.")
+
+    def showShip(self):
+        # This block has a lot of the code we've looked at.  Try to 
+        # see if you can understand how and why it makes the choices it does.
+        # 
+        # Why pick 20 to subtract from?  Then why 21 later?
+        # Are those the correct numbers?
+        # Why pick the integer division // instead of just a / ?
+        print("                   ___")
+        print("      ___....-----'---'-----....___")
+        if (len(self.shipname) > 0):
+            shipLine = " "
+            for x in range(20-(len(self.shipname)//2)):
+                shipLine = shipLine + "="
+            shipLine = shipLine + self.shipname
+            for x in range(21-(len(self.shipname)//2)):
+                shipLine = shipLine + "="
+            print(shipLine)
+        else:
+            print(" =========================================")
+        print("       ___'---..._______...---'___  ")
+        print("      (___)      _|_|_|_      (___)  ")
+        print("        \\____.-'_.---._'-.____//  ")
+        print("          cccc'.__'---'__.'cccc ")
+        print("                  ccccc")
 
 def makeSomeScreenSpace(howmany):
     # this routine should be pretty easy to understand.  It basically clears out 6 lines.
@@ -58,37 +74,45 @@ def boom(displayName):
     print("          | ;  :|")
     print(" _____.,-#%&$@%#&#~,._____")
 
+def transport(playerName):
+    if (playerName != "Cmdr. Kelly"):
+        print("omg transporter accident.  You are now part pig.")
+    else:
+        print("You are beamed up \n")
+
 def main():
     makeSomeScreenSpace(6)
     playerName = input("I am ready to be relieved, Commander...? ")
     playerName = "Cmdr. " + playerName
     print("Welcome to the bridge, " + playerName)
-    showShip("")
+    #
+    # This makes an object called myShip which is a instance of the Starship class above.
+    #
+    myShip = Starship(input("what is the name of your ship? "))
     print("\n")
     print("Right now I can't do much of anything, can I?")
     orders = "take command"
-    fighteraway = False
+
     while orders.lower() != 'quit':
         print("\n")
         orders = input("What are your orders " + playerName + " ")
         if orders.lower() == "rename":
-            nameTheShip = input("maybe you want to rename the ship, " + playerName +"?")
-            if (nameTheShip.upper()[:1] == 'Y'):
-                shipName = input("what should the name of the ship be? ")
-                showShip(shipName)
+            myShip.renameShip(input("what should the name of the ship be? "))
+            #
+            #  This was changed to call a method on the Starship class!
+            #
+            myShip.showShip()
+        if orders.lower() == "beam me up":
+            transport(playerName)
         if (orders.lower() == "self destruct" or orders.lower() == 'quit'):
-            boom(shipName)
+            boom(myShip.shipname)
             orders = 'quit'
         if orders.lower() == 'help':
-            print("I understand orders for Help, Rename, Self Destruct and quit ")
+            print("I understand orders for Help, Rename, Self Destruct, Beam me up, Launch Fighters, shoot and quit ")
         if orders.lower() == 'shoot':
             print("pew pew pew")
         if orders.lower() == 'launch fighters':
-            if (not fighteraway):
-                print("wing commander frank has launched with his entire wing.")
-                fighteraway = True
-            else :
-                print("Frank is long gone with the fighters")
+            myShip.launchFighters()
 
 
 if __name__ == "__main__":
